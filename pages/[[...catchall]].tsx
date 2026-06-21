361cb1f93c25816089ecc302effe4ec59c58a935
aogakid <aogakid@gmail.com>
2026-06-21 20:42:46 -0300

    apis inside pages

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -10,7 +10,34 @@ import type { GetStaticPaths, GetStaticProps } from "next";
 import Error from "next/error";
 import { useRouter } from "next/router";
 import { PLASMIC } from "@/plasmic-init";
-import { AllProviders } from "@/components/contexts/AppContext";
+import { AllProviders, useEditor, useApp, useEncaminha, useTimer, useGoogleSheets } from "@/components/contexts/AppContext";
+import type { EditorApi, AppApi, EncaminhaApi, TimerApi, GoogleSheetsApi } from "@/components/contexts/AppContext";
+
+function PlasmicApiWrapper({ children }: { children: React.ReactNode }) {
+  const editor = useEditor()
+  const app = useApp()
+  const enc = useEncaminha()
+  const timer = useTimer()
+  const sheets = useGoogleSheets()
+
+  React.useEffect(() => {
+    ;(window as unknown as { framerBloco?: EditorApi }).framerBloco = editor
+    ;(window as unknown as { framerApp?: AppApi }).framerApp = app
+    ;(window as unknown as { framerEncaminha?: EncaminhaApi }).framerEncaminha = enc
+    ;(window as unknown as { framerTimer?: TimerApi }).framerTimer = timer
+    ;(window as unknown as { framerGoogleSheets?: GoogleSheetsApi }).framerGoogleSheets = sheets
+
+    return () => {
+      delete (window as unknown as { framerBloco?: EditorApi }).framerBloco
+      delete (window as unknown as { framerApp?: AppApi }).framerApp
+      delete (window as unknown as { framerEncaminha?: EncaminhaApi }).framerEncaminha
+      delete (window as unknown as { framerTimer?: TimerApi }).framerTimer
+      delete (window as unknown as { framerGoogleSheets?: GoogleSheetsApi }).framerGoogleSheets
+    }
+  }, [editor, app, enc, timer, sheets])
+
+  return <>{children}</>
+}
 
 export default function PlasmicLoaderPage(props: {
   plasmicData?: ComponentRenderData;
@@ -37,19 +64,30 @@ export default function PlasmicLoaderPage(props: {
 
   return (
   <AllProviders>
-    <PlasmicRootProvider
-      loader={PLASMIC}
-      prefetchedData={plasmicData}
-      prefetchedQueryData={queryCache}
-      pageRoute={pageMeta.path}
-      pageParams={pageMeta.params}
-      pageQuery={router.query}
-      globalVariants={isDarkMode ? [{ name: "Mode", value: "dark" }] : []}
-    >
-      <PlasmicComponent component={pageMeta.displayName} />
-    </PlasmicRootProvider>
+    <PlasmicApiWrapper>
+      <PlasmicRootProvider
+        loader={PLASMIC}
+        prefetchedData={plasmicData}
+        prefetchedQueryData={queryCache}
+        pageRoute={pageMeta.path}
+        pageParams={pageMeta.params}
+        pageQuery={router.query}
+        globalVariants={isDarkMode ? [{ name: "Mode", value: "dark" }] : []}
+      >
+        <PlasmicComponent component={pageMeta.displayName} />
+      </PlasmicRootProvider>
+    </PlasmicApiWrapper>
   </AllProviders>
 );
+}
+
+// Attach APIs to window outside React for Plasmic access
+if (typeof window !== 'undefined') {
+  (window as any).framerBloco = { copiar: () => {}, colar: () => {}, substituir: () => {} }
+  (window as any).framerApp = { colarNoInput: () => {}, executarPrompt: () => {}, copiarOutput: () => {}, limparTudo: () => {} }
+  (window as any).framerEncaminha = { colarNoInput: () => {}, executarEncaminhamento: () => {}, copiarOutput: () => {}, limparTudo: () => {} }
+  (window as any).framerTimer = { ativarCronometro: () => {} }
+  (window as any).framerGoogleSheets = { enviarParaPlanilha: () => {} }
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
